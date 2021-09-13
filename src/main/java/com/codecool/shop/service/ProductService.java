27d7a9dcf532e92331a375577037f1463721e671
Twdor <adrianghitescu0713@gmail.com>
2021-09-13 18:08:12 +0300

    new field on ProductService -> supplierDao and getProductsBySupplierId(), getAllSuppliers() methods

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,18 +2,22 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 
 import java.util.List;
 
 public class ProductService{
     private ProductDao productDao;
     private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
 
-    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao) {
+    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao, SupplierDao supplierDao) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
+        this.supplierDao = supplierDao;
     }
 
     public ProductCategory getProductCategory(int categoryId){
@@ -24,6 +28,13 @@ public class ProductService{
         var category = productCategoryDao.find(categoryId);
         return productDao.getBy(category);
     }
+
+    public List<Product> getProductsBySupplierId(int supplierId) {
+        var supplier = supplierDao.find(supplierId);
+        return productDao.getBy(supplier);
+    }
+
+    public List<Supplier> getAllSuppliers() { return supplierDao.getAll(); }
 
     public List<ProductCategory> getAllProductCategories() { return productCategoryDao.getAll(); }
 
