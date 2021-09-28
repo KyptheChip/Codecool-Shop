fbd14583953e4d6cb1d146b5d3d948c8f8634ba9
CozminM <W@rdingboss00>
2021-09-28 13:12:51 +0300

    Add database connection manager

---
+++ src/main/java/com/codecool/shop/dao/inDatabase/DatabaseManager.java
@@ -0,0 +1,36 @@
+package com.codecool.shop.dao.inDatabase;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private ProductDao productDao;
+    private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
+
+
+    DataSource connect() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+        ApplicationProperties properties = new ApplicationProperties();
+
+        String dbName = properties.readProperty("database");
+        String user = properties.readProperty("user");
+        String password = properties.readProperty("password");
+
+        dataSource.setDatabaseName(dbName);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
+        return dataSource;
+    }
+}
